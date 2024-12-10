cba3b4f4f423bcb545515fa8b1bd997e23729f9e
mmokashi1 <mmokashi@binghamton.edu>
2024-12-09 19:30:02 -0500

    Initial commit with Recipe Book App

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,60 +1,67 @@
-import { EventEmitter, Injectable, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
-import { ShoppingListService } from '../shopping-list/shopping-list.service';
 import { Subject } from 'rxjs';
-import { DataStorageService } from '../shared/data-storage.service';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @Injectable()
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
-
-  // private recipes: Recipe[] = [
-  //   new Recipe(
-  //     'A test Recipe',
-  //     'This is simply a test',
-  //     'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272',
-  //     [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
-  //   ),
-  // ];
+  totalRecipesChanged = new Subject<number>(); // For pagination total recipes
 
   private recipes: Recipe[] = [];
+  private pageSize: number = 5;
 
-  constructor(
-    private slService: ShoppingListService
-  ) // private dataStorageService: DataStorageService
-  {}
+  constructor(private slService: ShoppingListService) {}
 
   setRecipes(recipes: Recipe[]) {
     this.recipes = recipes;
-    this.recipesChanged.next(this.recipes.slice());
+    this.recipesChanged.next(this.getPaginatedRecipes(1)); // Initialize with the first page
+    this.totalRecipesChanged.next(this.recipes.length);
   }
 
   getRecipes() {
     return this.recipes.slice();
   }
 
+  getPaginatedRecipes(page: number): Recipe[] {
+    const startIndex = (page - 1) * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+    return this.recipes.slice(startIndex, endIndex);
+  }
+
+  searchRecipes(query: string, page: number): Recipe[] {
+    const filteredRecipes = this.recipes.filter((recipe) =>
+      recipe.name.toLowerCase().includes(query.toLowerCase())
+    );
+    this.totalRecipesChanged.next(filteredRecipes.length); // Update total for pagination
+    const startIndex = (page - 1) * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+    return filteredRecipes.slice(startIndex, endIndex);
+  }
+
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
-    // this.slService.addIngredients(ingredients);
-    // this.dataStorageService.storeShoppingListIngredients(ingredients);
+    this.slService.addIngredients(ingredients);
   }
 
   getRecipe(index: number) {
-    return this.recipes.slice()[index];
+    return this.recipes[index];
   }
 
   addRecipe(recipe: Recipe) {
     this.recipes.push(recipe);
-    this.recipesChanged.next(this.recipes.slice());
+    this.recipesChanged.next(this.getPaginatedRecipes(1)); // Reset to first page
+    this.totalRecipesChanged.next(this.recipes.length); // Update total recipes
   }
 
   updateRecipe(index: number, newRecipe: Recipe) {
     this.recipes[index] = newRecipe;
-    this.recipesChanged.next(this.recipes.slice());
+    this.recipesChanged.next(this.getPaginatedRecipes(1)); // Reset to first page
   }
 
   deleteRecipe(index: number) {
     this.recipes.splice(index, 1);
-    this.recipesChanged.next(this.recipes.slice());
+    this.recipesChanged.next(this.getPaginatedRecipes(1)); // Reset to first page
+    this.totalRecipesChanged.next(this.recipes.length); // Update total recipes
   }
 }
